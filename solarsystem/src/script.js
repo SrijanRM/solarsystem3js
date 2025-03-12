c851d386b60a9b37369312ae72b3ef4f76e96382
unknown <srijancodex@gmail.com>
2025-03-12 20:16:41 +0530

    group children

--- solarsystem/src/script.js
+++ solarsystem/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { checker, rotate } from 'three/tsl';
 import { Pane } from "tweakpane";
 
 // initialize a pane
@@ -12,23 +13,32 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32)
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32)
+
+const group = new THREE.Group();
 
 // initilize material 
 const material = new THREE.MeshStandardMaterial();
 material.color = new THREE.Color("green")
 
-// pane.addInput(material ,'metalness' , {min:0,max:1,step:0.01})
-
-
 // initialze mesh
 const mesh = new THREE.Mesh(geometry, material);
-const mesh2 = new THREE.Mesh(torusKnotGeometry, material);
-mesh2.position.x = 1.5;
+
+const knot = new THREE.Mesh(torusKnotGeometry, material);
+knot.position.x = 1.5;
 
 const plane = new THREE.Mesh(planeGeometry, material);
 plane.position.x = -1.5;
 
-scene.add(mesh, mesh2, plane)
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.position.y = 1.5;
+
+const cylinder = new THREE.Mesh(cylinderGeometry, material);
+cylinder.position.y = -1.5;
+
+group.add(mesh, knot, plane, sphere, cylinder);
+scene.add(group)
 
 // initilize light 
 const light = new THREE.AmbientLight(0xffffff, 0.8);
@@ -61,7 +71,12 @@ window.addEventListener('resize', () => {
 
 
 const renderLoop = () => {
-  console.log("render loop here");
+  group.children.forEach((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.rotation.y += 0.01
+    }
+
+  })
   controls.update();
   renderer.render(scene, camera)
   window.requestAnimationFrame(renderLoop);
