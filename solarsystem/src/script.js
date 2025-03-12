d90ba5d1e10dae8e3993f07e68e09c283b210fe4
unknown <srijancodex@gmail.com>
2025-03-12 19:27:17 +0530

    material property
    fog , background color

--- solarsystem/src/script.js
+++ solarsystem/src/script.js
@@ -1,75 +1,32 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { log } from 'three/tsl';
+import { Pane } from "tweakpane";
+
+// initialize a pane
+const pane = new Pane();
 
 // initialize the scene
 const scene = new THREE.Scene()
 
-// add objects to the scene
-// const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-// const cubeMaterial1 = new THREE.MeshBasicMaterial({ color: "violet" })
+// initilize geomerty
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-// create custom geometry
-const vertices = new Float32Array([0, 0, 0, 0, 2, 0, 2, 0, 0])
-const bufferAttribute = new THREE.BufferAttribute(vertices, 3);
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position',bufferAttribute);
-const cubeMaterial2 = new THREE.MeshBasicMaterial({ color: "pink", wireframe: true });
-const cubeMesh = new THREE.Mesh(geometry, cubeMaterial2)
-scene.add(cubeMesh)
+// initilize material 
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0.5 });
 
-// const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial2)
-// scene.add(cubeMesh)
+const fog = new THREE.Fog(0xffffff,1,10);
+scene.fog = fog;
+scene.background = new THREE.Color('white')
+// initialze mesh
+const mesh = new THREE.Mesh(geometry, material);
+const mesh2 = new THREE.Mesh(geometry, material);
+mesh2.position.x = 1.5;
+scene.add(mesh)
+scene.add(mesh2)
 
-cubeMesh.rotation.y = 3;
 
-// const cubeMesh1 = new THREE.Mesh(cubeGeometry, cubeMaterial1)
-// const cubeMesh2 = new THREE.Mesh(cubeGeometry, cubeMaterial1)
-// const cubeMesh3 = new THREE.Mesh(cubeGeometry, cubeMaterial2)
-// const cubeMesh4 = new THREE.Mesh(cubeGeometry, cubeMaterial1)
-// const cubeMesh5 = new THREE.Mesh(cubeGeometry, cubeMaterial1)
-// const group = new THREE.Group();
-// group.add(cubeMesh1);
-// group.add(cubeMesh2);
-// group.add(cubeMesh3);
-// group.add(cubeMesh4);
-// group.add(cubeMesh5);
-// scene.add(group);
-
-// cubeMesh.position.y = 1;
-// cubeMesh.position.x = 1;
-// cubeMesh.position.z = 1;
-
-// cubeMesh1.position.y = 2;
-// cubeMesh1.position.x = 2;
-// cubeMesh1.position.z = 1;
-
-// cubeMesh2.position.y = 2;
-// cubeMesh2.position.x = 1;
-// cubeMesh2.position.z = 2;
-
-// cubeMesh3.position.y = 1;
-// cubeMesh3.position.x = 2;
-// cubeMesh3.position.z = 2;
-
-// cubeMesh4.position.y = 2;
-// cubeMesh4.position.x = 2;
-// cubeMesh4.position.z = 2;
-
-// cubeMesh5.position.y = 2;
-// cubeMesh5.position.x = 1;
-// cubeMesh5.position.z = 1;
-
-// const axesHelper = new THREE.AxesHelper(2)
-// scene.add(axesHelper);
-
-// const axesHelper = new THREE.AxesHelper(2)
-// cubeMesh.add(axesHelper);
-
-// initialize the camera , 2 type PerspectiveCamera and OrthographicCamera
+// initialize the camera 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30);
-// const aspectRatio = window.innerWidth / window.innerHeight;
-// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 200);
 camera.position.z = 5
 
 // initialize the renderer
@@ -77,14 +34,10 @@ const canvas = document.querySelector('canvas.threejs')
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-// smoothness
-// const maxPilxelRatio = Math.min(window.devicePixelRatio ,2);
-// renderer.setPixelRatio(maxPilxelRatio)
 
 // initilize controls 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-// controls.autoRotate = true;
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -92,21 +45,9 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
-// initilize clock
-const clock = new THREE.Clock();
-let previousTime = 0;
 
 const renderLoop = () => {
   console.log("render loop here");
-
-  // const currentTime = clock.getElapsedTime();
-  // const delta = currentTime - previousTime;
-  // previousTime = currentTime;
-  // cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * delta * 10;
-  // cubeMesh.position.x += delta;
-  // cubeMesh.scale.x += 1 * delta;
-  // cubeMesh.scale.x = Math.sin(currentTime) + 2;
-
   controls.update();
   renderer.render(scene, camera)
   window.requestAnimationFrame(renderLoop);
